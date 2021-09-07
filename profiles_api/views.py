d3c4a0ab2300b926c5cb26332bb0e3acf022e8f1
mvndinesh <mvn.mobileapps@gmail.com>
2021-09-07 01:27:56 -0500

    commited new files

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """ Test API View """
+
+    def get(self,request,format=None):
+        """Returns the list of API features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get,post,patch,put,delete)',
+            'Is similar to a traditional django view',
+            'Gives you the most control over the applicaton logic',
+            'Is mapped manually to the URLs',
+        ]
+        return Response({'message':'get method','an_apiview':an_apiview})
+
+
+
+
