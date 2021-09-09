7159967f07fd370892b17667e5cba1965ae90b14
mvndinesh <mvn.mobileapps@gmail.com>
2021-09-08 21:31:11 -0500

    My changes

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from profiles_api import views
 
 urlpatterns = [
-    path('hello_view/', views.HelloApiView.as_view()),
+    # path('/', views.HelloApiView.as_view()),
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """ Test API View """
+
+    serializer_class = serializers.HelloSerializers
 
     def get(self,request,format=None):
         """Returns the list of API features"""
@@ -14,6 +18,28 @@ class HelloApiView(APIView):
         ]
         return Response({'message':'get method','an_apiview':an_apiview})
 
+    def post(self, request):
+        """create hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            place = serializer.validated_data.get('place')
+            message = {"name":name, "place":place}
+            return Response(message)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
+    def put(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
 
+    def patch(self,request,pl=None):
+        """Handling a partial update of the object """
+        return Response({'method':'PATCH'})
 
+    def delete(self, request, pk=None):
+        """Deleting an object"""
+        return Response({'method':'Delete'})
