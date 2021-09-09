207b28406351dfe47a0f6e8720b91d124777f085
mvndinesh <mvn.mobileapps@gmail.com>
2021-09-08 23:54:00 -0500

    committed files

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -4,3 +4,4 @@ class HelloSerializers(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
     place = serializers.CharField(max_length=10)
+
