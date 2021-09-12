32e7d99a3e93ad60b24bfa3584b43a9b5c53754f
mvndinesh <mvn.mobileapps@gmail.com>
2021-09-12 17:45:15 -0500

    Added permissions file

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,6 +4,8 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+# base_name is not required for the User Profile View Set.
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
